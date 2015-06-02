db9142a0ea776f09b295b7f308e18d8f1445eb89
Diego Ramírez Milano <dl.ramirez1796@uniandes.edu.co>
2015-06-02 14:40:32 -0500

    organizados nombres de carpetas

--- Talleres/taller1(29-05-15)/reloj.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true
-do
-	date '+%H:%M:%S' | figlet
-	sleep 1s
-	clear
-done
--- Talleres/taller1(29-05-15)/taller.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-touch taller.csv
-for i in {1..1000}
-do 
-	(( a = i * i ))
-	echo $i,$a >> taller.csv
-done
-
-awk -F "," '{print $1","$2","$1+$2}' taller.csv > tallerawk.csv
