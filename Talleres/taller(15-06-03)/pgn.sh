05622a40fcbfd22b267ac0a5f0547ff4063a770d
Diego Ramírez Milano <dl.ramirez1796@uniandes.edu.co>
2015-06-03 22:37:38 -0500

    taller2 done

--- Talleres/taller(15-06-03)/pgn.sh
+++ Talleres/taller(15-06-03)/pgn.sh
@@ -12,4 +12,6 @@ sed -E -i 's/\t\ /\t/g' pgn.dat
 
 sed -E -i 's/\)//g' pgn.dat
 
-sed -E -i 's/\(/-/g' pgn.dat
+sed -E 's/\(/-/g' pgn.dat > pgn.tsv
+
+echo 'El sector con menor cambio porcentual fue: '$(sort --field-separator=$'\t' --key=4 -n pgn.tsv | awk -F"\t" '{print $1}' |head -1)
